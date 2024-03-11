78e119a9e5ce07df7daec6a29c875efe4b4037b0
mlisovich <maxim.lisovich@gmail.com>
2024-03-11 17:43:11 +0100

    PROXY-7 - changes after review

--- src/controllers/asteroidsController.js
+++ src/controllers/asteroidsController.js
@@ -12,12 +12,6 @@ const getAsteroids = async (req, res) => {
     }
 }
 
-const receivePictureFromRover = (req, res) => {
-    // TODO PROXY-11: Implement an endpoint of type POST
-    res.json({ WIP: 'Work-in-Progress' });
-}
-
 module.exports = {
-    getAsteroids,
-    receivePictureFromRover
+    getAsteroids
 };
---
+++ src/controllers/usersController.js
@@ -0,0 +1,8 @@
+const receivePictureFromRover = (req, res) => {
+    // TODO PROXY-11: Implement an endpoint of type POST
+    res.json({ WIP: 'Work-in-Progress' });
+}
+
+module.exports = {
+    receivePictureFromRover
+};
