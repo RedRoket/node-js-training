37b9223450b092d41082d2dd838a409d5c8c3c15
mlisovich <maxim.lisovich@gmail.com>
2024-03-14 12:54:21 +0100

    PROXY-12 - remove comments

--- src/routes/meteors.js
+++ src/routes/meteors.js
@@ -1,20 +1,11 @@
 const express = require('express');
-// const partialResponse = require('express-partial-response');
 const meteorsResponseMiddleware = require('../middleware/meteors-middleware');
 const validator = require('../middleware/validator-middleware');
 const getMeteors = require('../controllers/asteroids-controller');
 
 const router = express.Router();
 
-// using https://github.com/nemtsov/express-partial-response
-// from https://expressjs.com/en/resources/middleware.html
-// cheat way te get countOnly (example: http://localhost:4000/meteors?filter=elementCount)
 router
-    // .use(
-    //     partialResponse({
-    //         query: 'filter'
-    //     }))
-    .get('/', validator('asteroidsRequest'), meteorsResponseMiddleware, getMeteors)
-    .get('/test', getMeteors);
+    .get('/', validator('asteroidsRequest'), meteorsResponseMiddleware, getMeteors);
 
 module.exports = router;
